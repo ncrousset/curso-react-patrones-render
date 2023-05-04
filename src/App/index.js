66efce75c8543c7de77ccab1b536d2b585e73d2c
= <natanael926@gmail.com>
2023-05-04 12:45:10 -0400

    Composicion sin react context

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoProvider } from '../TodoContext';
 import { AppUI } from './AppUI';
 
 
 function App() {
+  const [state, setState] = useState("Hola mundo");
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </>  
   );
 }
+
+function TodoHeader({children}) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+
+function TodoList({children}) {
+  return (
+    <section className='todolist-container'>
+      {children}
+    </section>
+  ); 
+}
+
+function TodoCounter() {
+  return(
+    <p>Todo Counter</p>
+  );
+}
+
+function TodoSearch() {
+  return(
+    <p>Todo Search</p>
+  );
+}
+
+function TodoItem({state}) {
+  return <p>Todo Item: {state}</p>
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
